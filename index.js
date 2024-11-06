65b1ec220e8746289e4e1b9c58b89c98ea968489
chestakor <139700193+chestakor@users.noreply.github.com>
2024-11-07 01:39:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,24 +8,21 @@ module.exports = async (req, res) => {
       return res.status(400).json({ error: 'msisdn parameter is required' });
     }
 
-    const url = `https://app.mynagad.com:20002/api/user/check-user-status-for-log-in?msisdn=${msisdn}`;
+    // Updated URL
+    const url = `https://api.pikaapis.my.id/nagad.php?msisdn=${msisdn}`;
 
-    const headers = {
-      "X-KM-User-AspId": "100012345612345",
-      "X-KM-User-Agent": "ANDROID/1152",
-      "X-KM-DEVICE-FGP": "19DC58E052A91F5B2EB59399AABB2B898CA68CFE780878C0DB69EAAB0553C3C6",
-      "X-KM-Accept-language": "bn",
-      "X-KM-AppCode": "01"
-    };
+    // Send GET request
+    const response = await fetch(url);
 
-    // Make the API request
-    const response = await fetch(url, { headers });
-
+    // Check if the response is OK
     if (!response.ok) {
-      return res.status(response.status).json({ error: 'Failed to fetch user status' });
+      return res.status(response.status).json({ error: 'Failed to fetch user status from API' });
     }
 
+    // Parse the JSON response
     const data = await response.json();
+
+    // Send back the data from the API response
     return res.status(200).json(data);
 
   } catch (error) {
